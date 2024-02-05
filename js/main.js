1d5be0fb311413450b96705b40bbc20f2d1877ad
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-02-05 18:15:09 +0200

    Start home content

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var toggler = document.getElementById('toggler'),
+    sideNav = document.getElementById("side_nav"),
     sideContent = document.getElementById("side_content");
 
-toggler.addEventListener('click', function () {
 
-    [...toggler.children].forEach(sib => sib.classList.toggle('hidden_icon'))
+    toggler.addEventListener('click', function () {
 
-    sideContent.classList.toggle('show')
-    sideContent.classList.toggle('hide')
-})
+        [...toggler.children].forEach(sib => sib.classList.toggle('hidden_icon'))
+
+
+
+        sideNav.classList.toggle('show')
+        sideNav.classList.toggle('hide')
+        sideContent.classList.toggle('offset_left')
+    })
 
 function hiddenHandler(index) {
     this.children[index].style.display = 'none'
-
 }
