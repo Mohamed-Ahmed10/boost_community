84e65c0eb355e169bf73696e0975dc8b93b2d565
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-02-06 19:42:49 +0200

    Some responsive and tasks section

--- js/main.js
+++ js/main.js
@@ -3,15 +3,21 @@ var toggler = document.getElementById('toggler'),
     sideContent = document.getElementById("side_content");
 
 
-    toggler.addEventListener('click', function () {
+toggler.addEventListener('click', function () {
 
-        [...toggler.children].forEach(sib => sib.classList.toggle('hidden_icon'))
+    [...toggler.children].forEach(sib => sib.classList.toggle('hidden_icon'))
 
 
-        sideNav.classList.toggle('hide')
-        sideContent.classList.toggle('offset_left')
-    })
+    sideNav.classList.toggle('hide')
+    sideContent.classList.toggle('offset_left')
+})
 
 function hiddenHandler(index) {
     this.children[index].style.display = 'none'
+}
+
+///////////////////////////////////////////
+var loadingContainer = document.getElementById('loading_container')
+window.onload = function () {
+    loadingContainer.classList.add('hide')
 }
